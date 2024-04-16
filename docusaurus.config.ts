891a66e6c09bb952beeb11d516f35f9181a4a4fe
Jesse Redl <jredl@vendasta.com>
2024-04-16 08:09:10 -0600

    configure docsearch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -47,12 +47,39 @@ const config: Config = {
   ],
 
   themeConfig: {
+    algolia: {
+      // The application ID provided by Algolia
+      appId: '8SYZR207TF',
+
+      // Public API key: it is safe to commit it
+      apiKey: '62990849b8af71d78aaed947040b946e',
+
+      indexName: 'webpro',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: '',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: '/docs/', // or as RegExp: /\/docs\//
+        to: '/',
+      },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+    },
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'Website Pro Help',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Website Pro Logo',
         src: 'img/website-pro.svg',
       },
       items: [
