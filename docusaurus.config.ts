9cff85e84f1cd14666dd6e073d8a7b0e1e0dc592
Jesse Redl <jredl@vendasta.com>
2024-06-17 09:12:59 -0600

    remove vendasta links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -37,7 +37,7 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/vendasta/website-docs',
+          // editUrl: 'https://github.com/vendasta/website-docs',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -90,11 +90,6 @@ const config: Config = {
           sidebarId: 'tutorialSidebar',
           position: 'left',
           label: 'Getting Started',
-        },
-        {
-          href: 'https://github.com/vendasta/website-docs',
-          label: 'GitHub',
-          position: 'right',
         },
       ],
     },
