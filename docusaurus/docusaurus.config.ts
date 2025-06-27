53d0a4b01ee38beb5f7a0964af24108d0ab1ddbc
Jesse Redl <jredl@vendasta.com>
2025-06-27 14:40:30 -0600

    fix the website pro configuration

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/logo.png',
 
   // Set the production url of your site here
-  url: 'https://docs.website.io',
+  url: 'https://docs.websitepro.hosting',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
